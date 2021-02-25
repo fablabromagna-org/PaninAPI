cd218652f748eb20da04bb302ee229d192195efb
Edoardo Savini <4855649+esavini@users.noreply.github.com>
2021-02-25 22:50:45 +0100

    Fix

--- src/PaninApi.Abstractions/Dtos/InputItemDto.cs
+++ src/PaninApi.Abstractions/Dtos/InputItemDto.cs
@@ -1,11 +1,10 @@
-using System;
 using System.Text.Json.Serialization;
 
 namespace PaninApi.Abstractions.Dtos
 {
     public class InputItemDto
     {
-        [JsonPropertyName("item_id")] public Guid ItemId { get; set; }
+        [JsonPropertyName("item_id")] public int ItemId { get; set; }
         
         [JsonPropertyName("quantity")] public int Quantity { get; set; }
     }
